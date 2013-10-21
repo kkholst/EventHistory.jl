bfaea8c533e1d1a66091ad023c38f864fe1bd4c6
Klaus Holst <kkho@biostat.ku.dk>
2013-10-21 09:38:56 +0200

    examples...

--- README.md
+++ README.md
@@ -101,7 +101,7 @@ Ovarian cancer example (randomized trial)
 > ovarian["group"] = ovarian["rx"]-1;
 > ovarian["S"] = Event([:futime,:fustat],ovarian);
 
-> e = EventHistory.phreg(:(S~age+group),ovarian)
+> e = phreg(:(S~age+group),ovarian)
 
 Model: Cox,Surv :(~(1))
 
@@ -122,8 +122,8 @@ Simple example from the `survival` R-package
                 stop=[2,3,6,7,8,9,9,9,14,17],
                 event=[1,1,1,1,1,1,1,0,0,0],
                 x=[1,0,0,1,0,1,1,1,0,0])
-> d["S"] = EventHistory.Event([:start,:stop,:event],d);
-> e = EventHistory.phreg(:(S~x),d)
+> d["S"] = Event([:start,:stop,:event],d);
+> e = phreg(:(S~x),d)
 
 Model: Cox,Surv :(~(1))
 
--- examples/cox.jl
+++ examples/cox.jl
@@ -46,7 +46,7 @@ ovarian = data("survival", "ovarian");
 ovarian["group"] = ovarian["rx"]-1;
 ovarian["S"] = Event([:futime,:fustat],ovarian);
 
-e = EventHistory.phreg(:(S~age+group),ovarian)
+e = phreg(:(S~age+group),ovarian)
 
 ##################################################
 ### Cox regression with left truncation
@@ -55,5 +55,5 @@ d = DataFrame(start=[1,2,5,2,1,7,3,4,8,8],
               stop=[2,3,6,7,8,9,9,9,14,17],
               event=[1,1,1,1,1,1,1,0,0,0],
               x=[1,0,0,1,0,1,1,1,0,0]);
-d["S"] = EventHistory.Event([:start,:stop,:event],d);
-e = EventHistory.phreg(:(S~x),d)
+d["S"] = Event([:start,:stop,:event],d);
+e = phreg(:(S~x),d)
