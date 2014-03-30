d0af118904d8d9bca3b2cc98d5c5c26a1f81b207
Klaus Holst <kkho@biostat.ku.dk>
2014-03-30 20:53:11 +0200

    cox examples updated

--- examples/cox.jl
+++ examples/cox.jl
@@ -46,7 +46,7 @@ ovarian = dataset("survival", "ovarian");
 ovarian[:Group] = ovarian[:Rx].-1;
 ovarian[:S] = Event([:FUTime,:FUStat],ovarian);
 
-mm = phreg(S~Age+Group,ovarian);
+mm = phreg(S~Age+Group,ovarian)
 
 ## Prediction
 predict(mm,surv=false,X=[0 0]); ## Baseline
@@ -57,17 +57,13 @@ predict(mm,X=[56 1; 56 0],time=[600,100,400]) ## ... both groups
 using Winston
 plot(s56[:,1],s56[:,2])
 
-s = predict(mm,X=[56 1; 56 0],order=true)
-pr = DataFrame([[s[:,1:2]; s[:,[1,3]]] rep(["Group1","Group2"],each=size(s,1))],[:Time,:S,:Group])
+s = predict(mm,X=[56 1; 56 0],order=true);
+pr = DataFrame(Time=[s[:,1],s[:,1]], S=[s[:,2],s[:,3]], Group=rep(["Group1","Group2"],each=size(s,1)));
+
 using Gadfly
 p = plot(pr, x="Time", y="S",color="Group",
          Guide.ylabel("Survival probability"), Guide.title("Age 56"))        
 draw(PNG("surv.png",7inch,7inch),p)
-
-
-p = plot(x=s56[:,1],y=s56[:,2],
-         Guide.xlabel("Time"), Guide.ylabel("Survival probability"), Guide.title("Age 56, Group 1"));
-draw(PNG("surv.png",6inch,6inch),p)
 
 ##################################################
 ### Cox regression with left truncation
